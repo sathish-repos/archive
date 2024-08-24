545e0064f61d3247c1932e72f295f49225d47836
sathish <me.sathish.git@gmail.com>
2024-08-24 18:49:31 +0530

    add routing for subhashini resume app

--- apps/portfolio/shared/subhashini/main/src/app/app.routes.ts
+++ apps/portfolio/shared/subhashini/main/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Route } from '@angular/router';
+import { AboutComponent, HomeComponent } from '@anx/portfolio-shared-smart';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'about',
+    title: 'About',
+    loadComponent: () => AboutComponent,
+  },
+  {
+    path: '',
+    title: 'Subhashini.dev',
+    loadComponent: () => HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
