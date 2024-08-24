37200bf969220c216a8cc9b1539d696afab9064d
sathish <me.sathish.git@gmail.com>
2024-08-24 17:47:50 +0530

    create about-page in pages

---
+++ libs/portfolio/shared/ui/src/lib/pages/about-page/about-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'anx-about-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.scss',
+})
+export class AboutPageComponent {}
