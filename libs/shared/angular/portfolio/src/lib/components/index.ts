0a6fce86fd825548e15fc6465fd8669f0d29c9fd
sathish <me.sathish.git@gmail.com>
2024-08-24 20:17:55 +0530

    move nav to components folder

--- libs/shared/angular/portfolio/src/lib/components/index.ts
+++ libs/shared/angular/portfolio/src/lib/components/index.ts
@@ -1,3 +1,3 @@
 export * from './header-footer/mocks/header-footer.mocks';
 export * from './header-footer/models/header-footer.types';
-export * from './header-footer/nav/nav.component';
+export * from './header-footer/components/nav/nav.component';
