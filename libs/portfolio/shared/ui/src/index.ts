7107a2a2697c50ffba5e81e9d22569f2dda70d7f
sathish <me.sathish.git@gmail.com>
2024-08-25 20:39:54 +0530

    add mock to about page

--- libs/portfolio/shared/ui/src/index.ts
+++ libs/portfolio/shared/ui/src/index.ts
@@ -1,2 +1,5 @@
 export * from './lib/pages/about-page/about-page.component';
 export * from './lib/pages/home-page/home-page.component';
+
+export * from './lib/features/about/about-template/mocks/about-template.mocks';
+export * from './lib/features/about/about-template/models/about-template.types';
--- libs/portfolio/shared/ui/src/lib/pages/about-page/about-page.component.ts
+++ libs/portfolio/shared/ui/src/lib/pages/about-page/about-page.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutTemplateComponent } from '../../features/about/about-template/about-template.component';
+import { AboutTemplate } from '../../features/about/about-template/models/about-template.types';
+import { AboutTemplateMocks } from '../../features/about/about-template/mocks/about-template.mocks';
 
 @Component({
   selector: 'anx-about-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AboutTemplateComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.scss',
 })
-export class AboutPageComponent {}
+export class AboutPageComponent {
+  content = input<AboutTemplate>(AboutTemplateMocks);
+}
