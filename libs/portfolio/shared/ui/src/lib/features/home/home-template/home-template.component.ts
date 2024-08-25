4c23d792d20ed234a870cd8adc2dde9f9a1fd37f
sathish <me.sathish.git@gmail.com>
2024-08-25 22:53:32 +0530

    add home template mock to pages

--- libs/portfolio/shared/ui/src/lib/features/home/home-template/home-template.component.ts
+++ libs/portfolio/shared/ui/src/lib/features/home/home-template/home-template.component.ts
@@ -2,7 +2,6 @@ import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../hero/hero.component';
 import { HomeTemplate } from './models/home-template.types';
-import { HomeTemplateMocks } from './mocks/home-template.mocks';
 import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
@@ -13,5 +12,5 @@ import { ProjectsComponent } from '../projects/projects.component';
   styleUrl: './home-template.component.scss',
 })
 export class HomeTemplateComponent {
-  content = input<HomeTemplate>(HomeTemplateMocks);
+  content = input.required<HomeTemplate>();
 }
--- libs/portfolio/shared/ui/src/lib/pages/home-page/home-page.component.ts
+++ libs/portfolio/shared/ui/src/lib/pages/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeTemplateComponent } from '../../features/home/home-template/home-template.component';
+import { HomeTemplate } from '../../features/home/home-template/models/home-template.types';
+import { HomeTemplateMocks } from '../../features/home/home-template/mocks/home-template.mocks';
 
 @Component({
   selector: 'anx-home-page',
@@ -9,4 +11,6 @@ import { HomeTemplateComponent } from '../../features/home/home-template/home-te
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  content = input<HomeTemplate>(HomeTemplateMocks);
+}
