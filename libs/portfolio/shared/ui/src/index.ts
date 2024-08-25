6a92bbbf008e59cd4793cd02d1078ba09b7dc02f
sathish <me.sathish.git@gmail.com>
2024-08-25 22:05:39 +0530

    add home-template component to home-page

--- libs/portfolio/shared/ui/src/index.ts
+++ libs/portfolio/shared/ui/src/index.ts
@@ -3,3 +3,6 @@ export * from './lib/pages/home-page/home-page.component';
 
 export * from './lib/features/about/about-template/mocks/about-template.mocks';
 export * from './lib/features/about/about-template/models/about-template.types';
+
+export * from './lib/features/home/home-template/mocks/home-template.mocks';
+export * from './lib/features/home/home-template/models/home-template.types';
--- libs/portfolio/shared/ui/src/lib/pages/home-page/home-page.component.ts
+++ libs/portfolio/shared/ui/src/lib/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeTemplateComponent } from '../../features/home/home-template/home-template.component';
 
 @Component({
   selector: 'anx-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HomeTemplateComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
