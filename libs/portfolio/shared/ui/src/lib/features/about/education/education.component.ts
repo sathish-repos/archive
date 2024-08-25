71cf45e9f2f12409d1d01f98cdeb118213406867
sathish <me.sathish.git@gmail.com>
2024-08-25 20:36:23 +0530

    create education component

---
+++ libs/portfolio/shared/ui/src/lib/features/about/education/education.component.ts
@@ -0,0 +1,41 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Education } from '../about-template/models/about-template.types';
+
+@Component({
+  selector: 'anx-education',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './education.component.html',
+  styleUrl: './education.component.scss',
+})
+export class EducationComponent {
+  content = input.required<Education>();
+}
+
+export const EducationMocks = {
+  header: {
+    title: 'Education',
+    description: 'My educational qualifications',
+  },
+  schools: [
+    {
+      education: `B.E Computer Science and Engineering`,
+      name: `From: TCET`,
+      year: '2017-2021',
+      score: `76.5%`,
+    },
+    {
+      education: `Higher Sec School`,
+      name: `From: Govt. Hrs School`,
+      year: '2015-2017',
+      score: `87.25%`,
+    },
+    {
+      education: `High School`,
+      name: `From: Govt. High School`,
+      year: '2015-2017',
+      score: `76.6%`,
+    },
+  ],
+};
